19ff3c68c23039cdab364bf0a10293567dec26fc
Debasish Bhola <devop69@gmail.com>
2023-05-24 10:03:00 +0530

    Initial Commit

---
+++ Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    public static void main(String[] args) {
+       //ClassRoom of 500 students - You have to store the marks of 500 students
+       int [] marks = new int[500]; //The format- Declaration and memory allocation
+       marks[0] = 10;
+       marks[1] = 90;
+       marks[2] = 100;
+       marks[3] = 70;
+       marks[4] = 80;
+       marks[5] = 90;
+       marks[6] = 19;
+       marks[7] = 19;
+       System.out.println(marks[6]);
+       //Format2
+       int [] students = {100,32,393,333,3,3}; //Format- All together
+       System.out.println(students[3]);
+    }
+}
