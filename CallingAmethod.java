6a94d82591e6310261abd1a1b3cc14d76d9f0ead
Debasish Bhola <devop69@gmail.com>
2023-05-24 10:04:51 +0530

    Initial Commit

---
+++ CallingAmethod.java
@@ -0,0 +1,12 @@
+
+public class CallingAmethod {
+    static void myMethod(){
+        System.out.println("Acha thik hai smjh gaya");
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
