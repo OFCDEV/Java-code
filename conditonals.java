76a0b2b3828c2f0584f7dd836ab57508692063d4
Debasish Bhola <devop69@gmail.com>
2023-05-24 10:05:09 +0530

    Initial Commit

---
+++ conditonals.java
@@ -0,0 +1,20 @@
+public class conditonals {
+    public static void main(String[] args) {
+    int age = 19;
+    if (age>18){
+        System.out.println("Yes boy you can drive.");
+    }
+    else{
+        System.out.println("Boy you cant.");
+    }
+    //Logical Operators
+    boolean a = true;    
+    boolean b = false;    
+    if(a && b) {
+        System.out.println("Yes");
+    }
+    else{
+        System.out.println("No");
+    }
+    }
+}
